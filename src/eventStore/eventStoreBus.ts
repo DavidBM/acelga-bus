01785bbb2d938ae5ed3f66fb07be5a915d31735b
David Bonet Montes <webbonet@gmail.com>
2018-08-04 21:36:59 +0200

    add comment for eventstore

--- src/eventStore/eventStoreBus.ts
+++ src/eventStore/eventStoreBus.ts
@@ -3,6 +3,7 @@ import {Bus} from '../index';
  Design:
  - Class for pulling fron the http server
  - Factory class for recreating the Events
+ - Use npm i geteventstore-promise
  - Event base type that blocks the "name" property and forces to have a serializer class
  - Middleware that creates the event and blocks the execution
 */
